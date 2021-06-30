8d3977cc2fd699e74f9f52bc4b96b02d959d75ee
Obscur4ns <daniel.hall035@gmail.com>
2021-06-30 16:14:37 +0100

    made formatting changes

--- opsCalculator/ResultsApp.java
+++ opsCalculator/ResultsApp.java
@@ -3,15 +3,15 @@ package opsCalculator;
 public class ResultsApp {
 
 	public static void main(String[] args) {
-		System.out.println("Your physics results:");
+		System.out.print("Your physics results: ");
 		System.out.println(Results.Physics);
-		System.out.println("Your chemistry results:");
+		System.out.print("Your chemistry results: ");
 		System.out.println(Results.Chemistry);
-		System.out.println("Your biology results:");
+		System.out.print("Your biology results: ");
 		System.out.println(Results.Biology);
-		System.out.println("Your total marks are:");
+		System.out.print("Your total marks are: ");
 		System.out.println(Results.totalMarks());
-		System.out.println("Your average percentage is:");
+		System.out.print("Your average percentage is: ");
 		System.out.println(Results.resultsPercentage());
 		System.out.println("Well done, you're forgettable!");
 
